fdc2879064cc7402aec68bc84a4f9f1eac7e20ff
Tomas Martins <tomas.martins@dextra-sw.com>
2019-08-31 22:53:24 -0300

    Added prepareForReuse method

--- CSGOFloat/SkinHistoryCollectionViewCell.swift
+++ CSGOFloat/SkinHistoryCollectionViewCell.swift
@@ -13,4 +13,18 @@ class SkinHistoryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var floatLabel: UILabel!
     
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        initialSetup()
+    }
+    
+    func initialSetup() {
+        self.skinImageView.image = nil
+        self.nameLabel.text = ""
+        self.floatLabel.text = ""
+        self.layer.backgroundColor =  UIColor.clear.cgColor
+        self.layer.borderColor = UIColor.clear.cgColor
+        self.layer.borderWidth = 0.0
+    }
 }
