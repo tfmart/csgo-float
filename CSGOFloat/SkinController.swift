ba44e5b97eacb894e6aff365012dc1edd891c690
Tomas Martins <tomas.martins@dextra-sw.com>
2019-08-31 22:55:16 -0300

    Added documentation for setSkinName method

--- CSGOFloat/SkinController.swift
+++ CSGOFloat/SkinController.swift
@@ -40,6 +40,13 @@ class SkinController {
         }).resume()
     }
     
+    /**
+     Uses an weapon's info to format it's name just like it is in in-game and on Steam
+     
+     - Parameters:
+       - skin: The weapon object to get the information from
+     - Returns: A string with the weapon's formatted name
+    */
     func setSkinName(skin: WeaponSkin) -> String {
         var name: String = ""
         if skin.isVanilla() {
